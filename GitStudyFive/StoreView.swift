360bf8280d5e036b09edc138dc759bf983693a04
Ostap Artym <otapik125@gmail.com>
2024-06-29 22:36:33 +0300

    Added StoreView

---
+++ GitStudyFive/StoreView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreView.swift
+//  GitStudyFive
+//
+//  Created by Ostap Artym on 29.06.2024.
+//
+
+import SwiftUI
+
+struct StoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreView()
+}
