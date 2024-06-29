1de7971e32d85a3af9c3f98b4104b4dee4146b55
Ostap Artym <otapik125@gmail.com>
2024-06-29 22:41:14 +0300

    Added storeproperty view

---
+++ GitStudyFive/StorePropertyView.swift
@@ -0,0 +1,18 @@
+//
+//  StorePropertyView.swift
+//  GitStudyFive
+//
+//  Created by Ostap Artym on 29.06.2024.
+//
+
+import SwiftUI
+
+struct StorePropertyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StorePropertyView()
+}
