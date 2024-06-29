1346bd39dba019369cfde3f3e4946cd7d5b3b3ea
artym125 <otapik125@gmail.com>
2024-06-29 22:14:35 +0300

    Initial Commit

---
+++ GitStudyFive/GitStudyFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStudyFiveApp.swift
+//  GitStudyFive
+//
+//  Created by Ostap Artym on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GitStudyFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
