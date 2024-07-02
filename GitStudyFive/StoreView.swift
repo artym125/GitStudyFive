3a747fe14edf96cc57aba96d4bf9592f0b4572df
Ostap Artym <otapik125@gmail.com>
2024-07-03 00:28:17 +0300

    dddddfd11122

--- GitStudyFive/StoreView.swift
+++ GitStudyFive/StoreView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StoreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, dddd33!")
             .font(.title)
     }
 }
