012f1241f97d8f7f807620b9fce1dc411fb264ee
Ostap Artym <otapik125@gmail.com>
2024-07-03 00:27:04 +0300

    dddd

--- GitStudyFive/ContentView.swift
+++ GitStudyFive/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worlddddd!")
+            Text("Hello, worldddddddddd!")
                 .foregroundStyle(.white)
                 .font(.title)
         }
